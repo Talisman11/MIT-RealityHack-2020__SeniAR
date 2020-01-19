104965328d78478df219f0809cf73d036c6b4418
talisman11 <chen240sx@gmail.com>
2020-01-18 22:35:29 -0500

    Fix visuals, add sounds
    Fix tree height, lower scene, add delayed fade-in voice, bg music, bird and river sounds

---
+++ Assets/VoiceDelay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceDelay : MonoBehaviour
+{
+    public AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio.volume = 0.5f;
+        audio.PlayDelayed(5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (audio.volume < 1)
+        {
+            audio.volume += 0.1f * Time.deltaTime;
+        }
+    }
+}
